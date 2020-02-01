ab6380266039b19a880cba5f7abcfd98a96eebd7
Kuzaaa <r.kettemeyer@gmail.com>
2020-02-01 16:32:22 +0100

    update mot_vide()

--- automate_non_deterministe.c
+++ automate_non_deterministe.c
@@ -22,16 +22,13 @@ Automate_non_deterministe langage_vide(){
 //Renvoie un automate non déterministe reconnaissant le mot vide
 Automate_non_deterministe mot_vide(){
 	Automate_non_deterministe automate;
-	Etat etat;
-
-	etat.num = 0;
 
 	automate.alphabet = init_alphabet();
-	automate.liste_etat = alloc_tab_etat(1);
-	automate.liste_etat[0] = etat;
-	automate.etat_initial = etat;
+	automate.nombreEtats = 1;
+	automate.nombreEtatsFinaux = 1;
+	automate.etat_initial = 0;
 	automate.liste_etats_accepteurs = alloc_tab_etat(1);
-	automate.liste_etats_accepteurs[0] = etat;
+	automate.liste_etats_accepteurs[0] = 0;
 	automate.tab_transition = NULL;
 
 	return automate;
